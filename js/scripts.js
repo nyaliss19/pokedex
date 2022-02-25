c5035541e7deffd7e751825baaa5c9a24431e982
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-02-24 22:06:52 -0500

    Update scripts.js
    Wrote pokemonList -- an array of objects listing various Pokemon with their own data (name, height, and types)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'bulbasaur', height: 2.04, type: ['grass', 'poison'] }
+  { name: 'ivysaur', height: 3.03, type: ['grass', 'poison'] }
+  { name: 'venusaur', height: 6.07, type: ['grass', 'poison'] }
+  { name: 'charmander', height: 2, type: ['fire'] }
+  { name: 'charmeleon', height: 3.07, type: ['fire'] }
+  { name: 'charizard', height: 5.07, type: ['fire', 'flying'] }
+  { name: 'squirtle', height: 1.08, type: ['water'] }
+  { name: 'wartortle', height: 3.03, type: ['water'] }
+  { name: 'blastoise', height: 5.03, type: ['water'] }
+];
