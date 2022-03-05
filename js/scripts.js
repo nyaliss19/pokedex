d248d2cc7cec340f2d280c357e64efdcd8026547
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-03-04 21:58:40 -0500

    Refactored array into a IIFE
    Created an IIFE to house pokemonList to avoid accessing global state

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
-let pokemonList = [
-  { name: 'bulbasaur', height: 2.04, type: ['grass', 'poison'] },
-  { name: 'ivysaur', height: 3.03, type: ['grass', 'poison'] },
-  { name: 'venusaur', height: 6.07, type: ['grass', 'poison'] },
-  { name: 'charmander', height: 2, type: ['fire'] },
-  { name: 'charmeleon', height: 3.07, type: ['fire'] },
-  { name: 'charizard', height: 5.07, type: ['fire', 'flying'] },
-  { name: 'squirtle', height: 1.08, type: ['water'] },
-  { name: 'wartortle', height: 3.03, type: ['water'] },
-  { name: 'blastoise', height: 5.03, type: ['water'] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'bulbasaur', height: 2.04, type: ['grass', 'poison'] },
+    { name: 'ivysaur', height: 3.03, type: ['grass', 'poison'] },
+    { name: 'venusaur', height: 6.07, type: ['grass', 'poison'] },
+    { name: 'charmander', height: 2, type: ['fire'] },
+    { name: 'charmeleon', height: 3.07, type: ['fire'] },
+    { name: 'charizard', height: 5.07, type: ['fire', 'flying'] },
+    { name: 'squirtle', height: 1.08, type: ['water'] },
+    { name: 'wartortle', height: 3.03, type: ['water'] },
+    { name: 'blastoise', height: 5.03, type: ['water'] },
+  ];
 
-pokemonList.forEach(function(pokemon) {
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+      return pokemonList;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    };
+})();
+
   document.write("<p>" + pokemon.name + " (height : " + pokemon.height + ")");
 });
