4eeef5f9f67c8f76e87cfc9aa828599169eb3ba3
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-04-01 21:37:08 -0400

    Emptied previous array
    Emptied previous array to make way for API functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: 'bulbasaur', height: 2.04, type: ['grass', 'poison'] },
-    { name: 'ivysaur', height: 3.03, type: ['grass', 'poison'] },
-    { name: 'venusaur', height: 6.07, type: ['grass', 'poison'] },
-    { name: 'charmander', height: 2, type: ['fire'] },
-    { name: 'charmeleon', height: 3.07, type: ['fire'] },
-    { name: 'charizard', height: 5.07, type: ['fire', 'flying'] },
-    { name: 'squirtle', height: 1.08, type: ['water'] },
-    { name: 'wartortle', height: 3.03, type: ['water'] },
-    { name: 'blastoise', height: 5.03, type: ['water'] },
-  ];
+  let pokemonList = [];
 
   function add(pokemon) {
     pokemonList.push(pokemon);
