aed254d0700880483feb3cd5f027a9b49ed3c892
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-04-02 12:35:14 -0400

    Removed unnecessary code

--- js/scripts.js
+++ js/scripts.js
@@ -72,7 +72,3 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
