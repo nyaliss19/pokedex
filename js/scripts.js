7ee7f6b5c7b3b2e63e07b3a30debb837d52367b1
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-04-01 21:40:24 -0400

    added loadList function
    loadList pulls list of pokemon from API

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,21 @@ let pokemonRepository = (function () {
     mainList.appendChild(listItem);
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
   return {
     add: add,
     getAll: getAll,
